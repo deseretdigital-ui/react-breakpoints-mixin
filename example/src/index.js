27b9eb0ecf3fad38ec3da2b28c81937882a9cefb
Joshua Stoutenburg <jehoshua02@gmail.com>
2015-01-08 14:20:09 -0700

    Adds es5-shim/sham
    For IE8 support adds in es5-shim and sham.

--- example/src/index.js
+++ example/src/index.js
@@ -1,2 +1,4 @@
+require('../../bower_components/es5-shim/es5-shim.min.js');
+require('../../bower_components/es5-shim/es5-sham.min.js');
 require('./scss/styles.scss');
 module.exports.SimpleExample = require('./components/SimpleExample');
